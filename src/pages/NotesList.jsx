a32e812120b653196be29b285f54cc55da6c9648
Madhukanth T <madhu051196@gmail.com>
2021-08-07 20:42:49 +0530

    Added Modals for Notes and Tag

--- src/pages/NotesList.jsx
+++ src/pages/NotesList.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Modal from "react-modal";
 
 import SidebarItem from "../components/common/SidebarItem";
 import Divider from "../components/common/Divider";
 
-const Sidebar = () => {
+const Sidebar = ({ toggleTagAddModal }) => {
   const [activeState, setActiveState] = useState(0);
 
   const handleClick = (step) => () => {
@@ -34,7 +35,7 @@ const Sidebar = () => {
         <div className="h-2 w-2 rounded-lg bg-green-500" />
         <div className="ml-4">Personal</div>
       </SidebarItem>
-      <SidebarItem>
+      <SidebarItem handleClick={toggleTagAddModal}>
         <div className="text-4xl">+</div>
         <div className="ml-4">Add new</div>
       </SidebarItem>
@@ -77,8 +78,21 @@ const Card = () => {
   );
 };
 
+const ColorSelector = ({ color, active, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`h-8 w-8 mr-2 rounded-full cursor-pointer ${
+        active ? "bg-white" : `bg-${color}-500`
+      } border-solid border-${color}-500 border-4`}
+    />
+  );
+};
+
 function NotesList() {
   const [selectedTag, selectTag] = useState(0);
+  const [addModalOpen, setModalOpen] = useState(false);
+  const [addTagModalOpen, setTagModalOpen] = useState(false);
 
   const handleEnter = (e) => {
     if (e.keyCode !== 13) return;
@@ -89,13 +103,71 @@ function NotesList() {
     selectTag(step);
   };
 
-  const cards = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 43, 54, 5, 2, 2, 3, 33, 3, 3, 3, 3, 3, 3, 33,
-  ];
+  const toggleTagAddModal = () => {
+    setTagModalOpen(!addTagModalOpen);
+  };
+
+  const toggleAddModal = () => {
+    setModalOpen(!addModalOpen);
+  };
+
+  const cards = [1, 2, 3];
 
   return (
     <div className="w-screen h-screen flex">
-      <Sidebar />
+      <Sidebar toggleTagAddModal={toggleTagAddModal} />
+      <Modal
+        isOpen={addTagModalOpen}
+        style={{
+          overlay: {
+            background: "rgba(0,0,0,0.5)",
+          },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            maxWidth: "600px",
+            width: "100%",
+            borderRadius: "10px",
+          },
+        }}
+        contentLabel="Example Modal"
+      >
+        <div className="text-xl mb-4 font-medium text-primary">Add Tag</div>
+        <input
+          placeholder="Title"
+          className="w-full mb-4 p-2 border-gray-300 rounded-md border-solid border-2 focus:outline-none focus:border-primary"
+        />
+
+        <div className="flex mb-4">
+          <ColorSelector active color="red" />
+          <ColorSelector color="blue" />
+          <ColorSelector color="green" />
+          <ColorSelector color="yellow" />
+          <ColorSelector color="purple" />
+          <ColorSelector color="pink" />
+        </div>
+        <textarea
+          placeholder="Description"
+          rows={5}
+          className="w-full mb-4 p-2 border-gray-300 rounded-md border-solid border-2 focus:outline-none focus:border-primary"
+        />
+
+        <div className="flex items-center justify-end">
+          <button
+            className="text-primary focus:outline-none"
+            onClick={toggleTagAddModal}
+          >
+            Cancel
+          </button>
+          <button className="w-20 h-10 bg-primary text-white rounded-md ml-4 focus:outline-none">
+            Add
+          </button>
+        </div>
+      </Modal>
+
       <div className="h-full w-full-without-sidebar ">
         <div className="h-14 w-full shadow-md flex justify-center items-center">
           <input
@@ -122,12 +194,66 @@ function NotesList() {
               </TagButton>
             </div>
 
-            <button className="flex items-center">
+            <button
+              className="flex items-center focus:outline-none"
+              onClick={toggleAddModal}
+            >
               <div className="text-4xl border-solid rounded-full border-primary border-2 h-6 w-6 leading-5 text-primary">
                 +
               </div>
               <div className="ml-2 text-primary font-medium">Add new note</div>
             </button>
+
+            <Modal
+              isOpen={addModalOpen}
+              style={{
+                overlay: {
+                  background: "rgba(0,0,0,0.5)",
+                },
+                content: {
+                  top: "50%",
+                  left: "50%",
+                  right: "auto",
+                  bottom: "auto",
+                  transform: "translate(-50%, -50%)",
+                  maxWidth: "600px",
+                  width: "100%",
+                  borderRadius: "10px",
+                },
+              }}
+              contentLabel="Example Modal"
+            >
+              <div className="text-xl mb-4 font-medium text-primary">
+                Add Notes
+              </div>
+              <input
+                placeholder="Title"
+                className="w-full mb-4 p-2 border-gray-300 rounded-md border-solid border-2 focus:outline-none focus:border-primary"
+              />
+              <select className="w-full mb-4 p-2 border-gray-300 rounded-md border-solid border-2 focus:outline-none focus:border-primary">
+                <option>Project</option>
+                <option>Business</option>
+                <option>Personal</option>
+                <option>None</option>
+              </select>
+              <textarea
+                placeholder="Type notes here"
+                rows={5}
+                className="w-full mb-4 p-2 border-gray-300 rounded-md border-solid border-2 focus:outline-none focus:border-primary"
+              />
+
+              <div className="flex items-center justify-end">
+                <button
+                  className="text-primary focus:outline-none"
+                  onClick={toggleAddModal}
+                >
+                  Cancel
+                </button>
+                <button className="w-20 h-10 bg-primary text-white rounded-md ml-4 focus:outline-none">
+                  Add
+                </button>
+              </div>
+            </Modal>
           </div>
 
           <div className="flex flex-wrap w-full overflow-y-scroll overflow-x-hidden max-h-full-without-header-tags mt-2">
